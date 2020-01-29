ea25b4de14db436d197f6f44c9df4adb5350a3ba
Rafa <zoomik100@gmail.com>
2020-01-29 09:07:23 +0100

    changed livedata exposure in VM

--- app/src/main/java/one/gypsy/neatorganizer/presentation/people/vm/PeopleViewModel.kt
+++ app/src/main/java/one/gypsy/neatorganizer/presentation/people/vm/PeopleViewModel.kt
@@ -9,8 +9,10 @@ import javax.inject.Inject
 //TODO Change the way of exposing LiveData https://gist.github.com/humblehacker/0eb6458b1df6cf3049e031f36f0615f5
 class PeopleViewModel @Inject constructor(var getAllPeopleUseCase: GetAllPeople) : ViewModel() {
 
-    val people: LiveData<List<Person>> = MediatorLiveData()
-
+    private val _people = MediatorLiveData<List<Person>>()
+    val people: LiveData<List<Person>>
+        get() = _people
+    
     init {
         getAllPeopleUseCase.invoke(viewModelScope, Unit) {
             it.either(::onGetAllPeopleFailure, :: onGetAllPeopleSuccess)
@@ -22,8 +24,8 @@ class PeopleViewModel @Inject constructor(var getAllPeopleUseCase: GetAllPeople)
     }
 
     private fun onGetAllPeopleSuccess(peopleCollectionSource: LiveData<List<Person>>) {
-        (people as MediatorLiveData).addSource(peopleCollectionSource) {
-            people.postValue(it)
+        _people.addSource(peopleCollectionSource) {
+            _people.postValue(it)
         }
     }
 }
