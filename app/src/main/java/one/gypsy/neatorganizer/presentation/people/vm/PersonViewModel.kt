c54cef1172f3d171bc92168e4e47929650ba017d
Rafa <zoomik100@gmail.com>
2020-01-29 08:48:46 +0100

    Update PersonViewModel.kt

--- app/src/main/java/one/gypsy/neatorganizer/presentation/people/vm/PersonViewModel.kt
+++ app/src/main/java/one/gypsy/neatorganizer/presentation/people/vm/PersonViewModel.kt
@@ -4,7 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import one.gypsy.neatorganizer.domain.Person
-
+//TODO Change the way of exposing LiveData https://gist.github.com/humblehacker/0eb6458b1df6cf3049e031f36f0615f5
 class PersonViewModel: ViewModel() {
 
     val person: LiveData<Person> = MutableLiveData<Person>()
