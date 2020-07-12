ae847fd7949a0cc30def342f6600ef97cd5a46e6
gypsy0ne <zoomik100@gmail.com>
2020-07-12 19:25:32 +0200

    Minor project clean up

--- app/src/main/java/one/gypsy/neatorganizer/utils/extensions/KotlinExtension.kt
+++ app/src/main/java/one/gypsy/neatorganizer/utils/extensions/KotlinExtension.kt
@@ -1,21 +1,13 @@
 package one.gypsy.neatorganizer.utils.extensions
 
-import android.content.Context
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
-import android.widget.Toast
 import androidx.core.content.ContextCompat.getSystemService
-import androidx.fragment.app.Fragment
 import androidx.lifecycle.MutableLiveData
 import java.util.*
 
-
 fun <T : Any?> MutableLiveData<T>.default(initialValue: T) = apply { setValue(initialValue) }
-
-fun Fragment.showToast(context: Context, stringId: Int) {
-    Toast.makeText(context, stringId, Toast.LENGTH_SHORT).show()
-}
 
 fun View.hide() {
     this.visibility = View.GONE
