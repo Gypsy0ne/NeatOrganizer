8acf4a010e37bbf0fc4147f2972ed7feaed3c088
gypsy0ne <zoomik100@gmail.com>
2021-01-31 14:22:47 +0100

    cr fixes

--- app/src/main/java/one/gypsy/neatorganizer/presentation/tasks/vm/TaskWidgetContentManageViewModel.kt
+++ app/src/main/java/one/gypsy/neatorganizer/presentation/tasks/vm/TaskWidgetContentManageViewModel.kt
@@ -71,10 +71,7 @@ class TaskWidgetContentManageViewModel(
                     )
                 )
             ) {
-                it.either(
-                    {},
-                    {}
-                )
+                it.either({}, {})
             }
         }
     }
