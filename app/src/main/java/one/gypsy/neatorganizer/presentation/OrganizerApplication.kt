6f2af377a2979bacf9afe4b67310726acc8b63d8
gypsy0ne <zoomik100@gmail.com>
2021-03-13 21:33:03 +0100

    release changes

--- app/src/main/java/one/gypsy/neatorganizer/presentation/OrganizerApplication.kt
+++ app/src/main/java/one/gypsy/neatorganizer/presentation/OrganizerApplication.kt
@@ -1,6 +1,9 @@
 package one.gypsy.neatorganizer.presentation
 
+import android.app.Activity
 import android.app.Application
+import android.content.pm.ActivityInfo
+import android.os.Bundle
 import one.gypsy.neatorganizer.data.notesDataSourceModule
 import one.gypsy.neatorganizer.data.routinesDataSourceModule
 import one.gypsy.neatorganizer.data.routinesResetDataSourceModule
@@ -55,5 +58,25 @@ internal class OrganizerApplication : Application() {
                 noteViewModelModule,
             )
         }
+
+        registerActivityLifecycleCallbacks(createLifecycleCallbacks())
+    }
+
+    private fun createLifecycleCallbacks() = object : ActivityLifecycleCallbacks {
+        override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
+            activity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+        }
+
+        override fun onActivityStarted(activity: Activity) {}
+
+        override fun onActivityResumed(activity: Activity) {}
+
+        override fun onActivityPaused(activity: Activity) {}
+
+        override fun onActivityStopped(activity: Activity) {}
+
+        override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) {}
+
+        override fun onActivityDestroyed(activity: Activity) {}
     }
 }
--- buildSrc/src/main/java/Version.kt
+++ buildSrc/src/main/java/Version.kt
@@ -6,8 +6,8 @@ object Version {
     const val compileSdkVersion = 29
     const val minSdkVersion = 26
     const val targetSdkVersion = 29
-    const val versionCode = 1
-    const val versionName = "1.0"
+    const val versionCode = 3
+    const val versionName = "3.0"
     const val jvmTarget = "1.8"
 
     // Libs
